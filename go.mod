2f1a14650726e579f6feb59edba6ab396f45e8b5
Fernandez Ludovic <lfernandez.dev@gmail.com>
2019-02-19 01:45:41 +0100

    refactor: use ghactions.

--- go.mod
+++ go.mod
@@ -3,5 +3,6 @@ module github.com/ldez/gha-mjolnir
 require (
 	github.com/google/go-github v17.0.0+incompatible
 	github.com/google/go-github/v24 v24.0.0
-	golang.org/x/oauth2 v0.0.0-20180821212333-d2e6202438be
+	github.com/ldez/ghactions v1.0.0
+	golang.org/x/oauth2 v0.0.0-20190212230446-3e8b2be13635
 )
--- main.go
+++ main.go
@@ -1,12 +1,31 @@
 package main
 
-import "log"
+import (
+	"context"
+	"log"
+
+	"github.com/google/go-github/v24/github"
+	"github.com/ldez/ghactions"
+)
 
 func main() {
 	displayVersion()
 
-	err := action(false)
+	err := ghactions.NewAction(context.Background()).OnPullRequest(action).Run()
 	if err != nil {
 		log.Fatal(err)
 	}
+}
+
+func action(client *github.Client, event *github.PullRequestEvent) error {
+	action := event.GetAction()
+
+	if action != "closed" || !event.PullRequest.GetMerged() {
+		log.Printf("skip: %q merge %v", action, event.PullRequest.GetMerged())
+		return nil
+	}
+
+	owner, repoName := ghactions.GetRepoInfo()
+
+	return closeRelatedIssues(context.Background(), client, owner, repoName, event.PullRequest, false)
 }
